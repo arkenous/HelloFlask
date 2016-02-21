7f32b3cd125237e2e5ab525dc50a9d45317c1f63
Kensuke Kosaka <k@trileg.net>
2016-02-21 15:38:00 +0900

    Add Ajax POST sample

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template
+from flask import request, jsonify
+import json
 
 app = Flask(__name__)
 
@@ -9,6 +11,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/postText', methods=['POST'])
+def lower_conversion():
+    text = request.json['text']
+    if "ping" in text:
+        return_data = {"result":"pong"}
+        return jsonify(ResultSet=json.dumps(return_data))
+    lower_text = text.lower()
+    return_data = {"result":lower_text}
+    return jsonify(ResultSet=json.dumps(return_data))
 
 
 if __name__ == '__main__':
--- FlaskApp/static/js/sample.js
+++ FlaskApp/static/js/sample.js
@@ -2,6 +2,17 @@ $(window).load(init());
 
 function init() {
   $("#button").click(function() {
-    $("#hello").text("Yeah!");
+    var textData = JSON.stringify({"text":$("#input-text").val()});
+    $.ajax({
+      type:'POST',
+      url:'/postText',
+      data:textData,
+      contentType:'application/json',
+      success:function(data) {
+        var result = JSON.parse(data.ResultSet).result;
+        $("#hello").text(result);
+        }
+    });
+    return false;
   });
 }
